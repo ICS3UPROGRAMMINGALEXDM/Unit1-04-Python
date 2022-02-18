c752b905702300c38b2f811d64af4b53a7bd6b27
Ubuntu <ubuntu@ip-172-31-86-29.ec2.internal>
2022-02-18 14:55:23 +0000

    finished area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created By: Alex De Meo
+# Date: 02//22
+# Description: calculates the area and perimeter of a rectangle
+
+
+def calculate():
+    print("Make sure to enter numbers only!")
+    num1 = input("What is the height of your retangle? \n").strip()
+    num2 = input("What is the width of your rectangle?\n").strip()
+    height = int(num1)
+    width = int(num2)
+    perimeter = (2*height) + (2 * width)
+    area = height * width
+    print("The perimeter of your rectangle is {} u. ".format(perimeter))
+    print("The area of your rectangle is {} u^2!".format(area))
+
+
+def main():
+    calculate()
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created By: Alex De Meo
+# Date: 02//22
+# Description: Displays basic math operations
+
+
+import math
+
+
+add = 5 + 3
+sub = 17-9
+times = 12 * 13
+div = 11 / 2
+power = pow(4, 3)
+square = math.sqrt(144)
+print("5 + 3 = {}, \n\n17 - 9 = {},".format(add, sub,))
+print("\n\n12 x 13 = {},".format(times))
+print("\n\n11 / 2 = {}, \n\n4^3 = {},".format(div, power))
+print("\n\n2_/144 = {} ".format(square))
