4b94e49a3a7f9c145dc415d5cec2fe92a30850e0
Ubuntu <ubuntu@ip-172-31-86-29.ec2.internal>
2022-02-23 00:43:48 +0000

    Reformatted basic_math.py

--- basic_math.py
+++ basic_math.py
@@ -9,12 +9,17 @@ import math
 
 
 add = 5 + 3
-sub = 17-9
+sub = 17 - 9
 times = 12 * 13
 div = 11 / 2
 power = pow(4, 3)
 square = math.sqrt(144)
-print("5 + 3 = {}, \n\n17 - 9 = {},".format(add, sub,))
+print(
+    "5 + 3 = {}, \n\n17 - 9 = {},".format(
+        add,
+        sub,
+    )
+)
 print("\n\n12 x 13 = {},".format(times))
 print("\n\n11 / 2 = {}, \n\n4^3 = {},".format(div, power))
 print("\n\n2_/144 = {} ".format(square))
